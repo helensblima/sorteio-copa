2a6c171bfab496b0c4146fad2d4493867b55a102
Sasa <helensblima@gmail.com>
2026-03-26 17:08:46 -0300

    feat: add routes and layout with outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { AppRoutes } from './routes'
+
 function App() {
-  return <h1 className='app-title'>Simulador de Sorteio da Copa do Mundo!</h1>
+  return <AppRoutes />
 }
 
 export default App
---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router'
+import { Layout } from '@/components/Layout'
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<h1>Home</h1>} />
+        <Route path='configuracao-sorteio' element={<h1>Configure o Sorteio</h1>} />
+        <Route path='escolha-selecoes' element={<h1>Escolha os países</h1>} />
+        <Route path='resultado-sorteio' element={<h1>Resultado do Sorteio </h1>} />
+      </Route>
+    </Routes>
+  )
+}
