4dda240e7b3f29c57a15d8870ad800f0bc5deb97
Sasa <helensblima@gmail.com>
2026-03-29 01:40:52 -0300

    feat: add country flags

--- src/constants/flags.ts
+++ src/constants/flags.ts
@@ -1,204 +1,67 @@
-import braFlag from '@/assets/images/bra.svg'
+import BRA from '@/assets/images/BRA.svg'
+import ARG from '@/assets/images/ARG.svg'
+import URU from '@/assets/images/URU.svg'
+import COL from '@/assets/images/COL.svg'
+import ECU from '@/assets/images/ECU.svg'
+import CHI from '@/assets/images/CHI.svg'
+import CAN from '@/assets/images/CAN.svg'
+import MEX from '@/assets/images/MEX.svg'
+import USA from '@/assets/images/USA.svg'
+import CRC from '@/assets/images/CRC.svg'
+import FRA from '@/assets/images/FRA.svg'
+import ENG from '@/assets/images/ENG.svg'
+import ESP from '@/assets/images/ESP.svg'
+import GER from '@/assets/images/GER.svg'
+import POR from '@/assets/images/POR.svg'
+import ITA from '@/assets/images/ITA.svg'
+import NED from '@/assets/images/NED.svg'
+import BEL from '@/assets/images/BEL.svg'
+import CRO from '@/assets/images/CRO.svg'
+import DEN from '@/assets/images/DEN.svg'
+import SUI from '@/assets/images/SUI.svg'
+import POL from '@/assets/images/POL.svg'
+import SRB from '@/assets/images/SRB.svg'
+import JPN from '@/assets/images/JPN.svg'
+import KOR from '@/assets/images/KOR.svg'
+import AUS from '@/assets/images/AUS.svg'
+import IRN from '@/assets/images/IRN.svg'
+import KSA from '@/assets/images/KSA.svg'
+import MAR from '@/assets/images/MAR.svg'
+import SEN from '@/assets/images/SEN.svg'
+import NGA from '@/assets/images/NGA.svg'
+import CMR from '@/assets/images/CMR.svg'
 
 export const FLAGS = [
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
-  {
-    src: braFlag,
-    alt: 'Brasil',
-  },
+  { src: BRA, alt: 'Bandeira do Brasil' },
+  { src: ARG, alt: 'Bandeira da Argentina' },
+  { src: URU, alt: 'Bandeira do Uruguai' },
+  { src: COL, alt: 'Bandeira da Colômbia' },
+  { src: ECU, alt: 'Bandeira do Equador' },
+  { src: CHI, alt: 'Bandeira do Chile' },
+  { src: CAN, alt: 'Bandeira do Canadá' },
+  { src: MEX, alt: 'Bandeira do México' },
+  { src: USA, alt: 'Bandeira dos Estados Unidos' },
+  { src: CRC, alt: 'Bandeira da Costa Rica' },
+  { src: FRA, alt: 'Bandeira da França' },
+  { src: ENG, alt: 'Bandeira da Inglaterra' },
+  { src: ESP, alt: 'Bandeira da Espanha' },
+  { src: GER, alt: 'Bandeira da Alemanha' },
+  { src: POR, alt: 'Bandeira de Portugal' },
+  { src: ITA, alt: 'Bandeira da Itália' },
+  { src: NED, alt: 'Bandeira da Holanda' },
+  { src: BEL, alt: 'Bandeira da Bélgica' },
+  { src: CRO, alt: 'Bandeira da Croácia' },
+  { src: DEN, alt: 'Bandeira da Dinamarca' },
+  { src: SUI, alt: 'Bandeira da Suíça' },
+  { src: POL, alt: 'Bandeira da Polônia' },
+  { src: SRB, alt: 'Bandeira da Sérvia' },
+  { src: JPN, alt: 'Bandeira do Japão' },
+  { src: KOR, alt: 'Bandeira da Coreia do Sul' },
+  { src: AUS, alt: 'Bandeira da Austrália' },
+  { src: IRN, alt: 'Bandeira do Irã' },
+  { src: KSA, alt: 'Bandeira da Arábia Saudita' },
+  { src: MAR, alt: 'Bandeira do Marrocos' },
+  { src: SEN, alt: 'Bandeira do Senegal' },
+  { src: NGA, alt: 'Bandeira da Nigéria' },
+  { src: CMR, alt: 'Bandeira de Camarões' },
 ]
